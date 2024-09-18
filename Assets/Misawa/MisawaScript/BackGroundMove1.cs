490d1add89a98b1b46ecd72a86624b5b333b78ec
misawahayato <2220034@ecc.ac.jp>
2024-09-18 17:51:03 +0900

    背景ループ追加。

---
+++ Assets/Misawa/MisawaScript/BackGroundMove1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMove1 : MonoBehaviour
+{
+    private float speed = 2;
+
+    void Update()
+    {
+        transform.position -= new Vector3(Time.deltaTime * speed + MasterSpeed.SpeedControl, 0f);
+
+        if (transform.position.x <= -19.2)
+        {
+            transform.position = new Vector3(38.4f, 0f);
+        }
+    }
+}
