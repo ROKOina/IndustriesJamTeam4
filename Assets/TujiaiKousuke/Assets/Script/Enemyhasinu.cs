d5c3d62bc603d6159094d069ccaafb07c42a6cec
misawahayato <2220034@ecc.ac.jp>
2024-09-19 13:11:58 +0900

    中間発表用に修正

--- Assets/TujiaiKousuke/Assets/Script/Enemyhasinu.cs
+++ Assets/TujiaiKousuke/Assets/Script/Enemyhasinu.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BOSS : MonoBehaviour
+public class Enemyhasinu : MonoBehaviour
 {
-    public float speed =0.6f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")//“G‚É“–‚½‚Á‚½Žž
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
