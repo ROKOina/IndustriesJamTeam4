65851d8526be55d27010ca2c2310c2066914228f
misawahayato <2220034@ecc.ac.jp>
2024-09-20 00:21:47 +0900

    スコア設定とリザルトの得点表示

--- Assets/TujiaiKousuke/Assets/Script/seni.cs
+++ Assets/TujiaiKousuke/Assets/Script/seni.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class seni : MonoBehaviour
 {
+    public float minAlpha = 0.2f;    // 最小の透明度
+    public float maxAlpha = 1.0f;    // 最大の透明度
+    public float blinkSpeed = 1.0f;  // 点滅の速さ
+    private Renderer objectRenderer;
+    private bool increasingAlpha = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 100;
+        objectRenderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -18,6 +24,32 @@ public class seni : MonoBehaviour
             MasterSpeed.SpeedControl = 0;
             SceneManager.LoadScene("Title");
         }
+        // 現在の透明度を取得
+        Color currentColor = objectRenderer.material.color;
 
+
+        // 透明度を徐々に変化させる
+        float alphaChange = blinkSpeed * Time.deltaTime;
+        if (increasingAlpha)
+        {
+            currentColor.a += alphaChange;
+            if (currentColor.a >= maxAlpha)
+            {
+                currentColor.a = maxAlpha;
+                increasingAlpha = false;
+            }
+        }
+        else
+        {
+            currentColor.a -= alphaChange;
+            if (currentColor.a <= minAlpha)
+            {
+                currentColor.a = minAlpha;
+                increasingAlpha = true;
+            }
+        }
+
+        // 新しい透明度をマテリアルに適用
+        objectRenderer.material.color = currentColor;
     }
 }
