f5c3207955e7e83bf1c76fa2ee6c3ed4d42c32fa
misawahayato <2220034@ecc.ac.jp>
2024-09-19 14:01:51 +0900

    色々追加

--- Assets/TujiaiKousuke/Assets/Script/seni.cs
+++ Assets/TujiaiKousuke/Assets/Script/seni.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class BOSS : MonoBehaviour
+using UnityEngine.SceneManagement;
+public class seni : MonoBehaviour
 {
-    public float speed =0.6f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +15,8 @@ public class BOSS : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-         
-
-
+            SceneManager.LoadScene("Title");
         }
-        transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+
     }
 }
